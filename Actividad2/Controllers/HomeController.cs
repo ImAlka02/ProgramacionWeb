4968c9efc0fea6dff44e41ee7d3c848cc5182f6b
ImAlka02 <ajiju.2000@hotmail.com>
2023-09-28 21:16:23 -0600

    Actividad 2 agregada

---
+++ Actividad2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Actividad2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Convertir(MonedaViewModel vm) 
+        {
+            if(vm.Moneda == "mx" || vm.Moneda == "MX")
+            {
+                vm.Conversion = vm.Valor * (decimal)18.00;
+            } else if(vm.Moneda == "usd" || vm.Moneda == "USD")
+            {
+                vm.Conversion = vm.Valor / (decimal)18.00;
+            }
+            return View(vm);  
+        }
+    }
+}
---
+++ Actividad2/Models/MonedaViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Actividad2.Models
+{
+    public class MonedaViewModel
+    {
+        public decimal Valor { get; set; }
+        public decimal Conversion { get; set; }
+        public string? Moneda { get; set; }
+    }
+}
