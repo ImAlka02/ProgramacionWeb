1fe96128641bcf809b65f965acbb92afdd4bfa65
ImAlka02 <ajiju.2000@hotmail.com>
2023-10-04 01:41:52 -0600

    Trabajo terminado

--- Act1ZooPlanet/Controllers/EspeciesController.cs
+++ Act1ZooPlanet/Controllers/EspeciesController.cs
@@ -1,5 +1,7 @@
 ﻿using Act1ZooPlanet.Models.Entities;
+using Act1ZooPlanet.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Act1ZooPlanet.Controllers
 {
@@ -9,8 +11,38 @@ namespace Act1ZooPlanet.Controllers
         {
             AnimalesContext context = new();
 
-            var datos = context.
-            return View();
+            var datos = context.Clase.Include(x => x.Especies)
+                .Select(x => new EspeciesViewModel()
+                {
+                    IdClase = x.Id,
+                    NombreClase = x.Nombre ?? "Sin nombre",
+                    Especie = x.Especies.Select(x => new EspecieModel()
+                    {
+                        IdEspecie = x.Id,
+                        NombreEspecie = x.Especie
+                    })
+                });
+            
+            return View(datos);
+        }
+
+        public IActionResult Especie(string Id) 
+        {
+            Id = Id.Replace("-", " ");
+
+            AnimalesContext context = new();
+
+            var datos = context.Especies.Where(x => x.Especie == Id).Include(x => x.IdClaseNavigation).Select(x=> new EspecieViewModel()
+            {
+                Id = x.Id,
+                Nombre = x.Especie,
+                Clase = x.IdClaseNavigation.Nombre,
+                Habitad = x.Habitat,
+                Observaciones = x.Observaciones,
+                Peso = x.Peso,
+                Size = x.Tamaño
+            }).FirstOrDefault();
+            return View(datos);
         }
     }
 }
---
+++ Act1ZooPlanet/Models/ViewModels/EspecieViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Act1ZooPlanet.Models.ViewModels
+{
+    public class EspecieViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Clase { get; set; }
+        public double? Peso { get; set; }
+        public int? Size { get; set; }
+        public string Habitad { get; set; }
+        public string Observaciones { get; set;}
+    }
+}
